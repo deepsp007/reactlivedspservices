2c0bdf123013be478fa46fbf7dafe217df3ce701
deepsp007 <deepsp007.ds@gmail.com>
2021-05-07 17:54:32 +0530

    Updates

---
+++ precache-manifest.fc55b432087aa09a9e508ee30ff31b8d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bf226b69f2f2e55afba",
+    "url": "/reactlivedspservices/static/css/main.3ba5317f.chunk.css"
+  },
+  {
+    "revision": "4bf226b69f2f2e55afba",
+    "url": "/reactlivedspservices/static/js/main.7924eb22.chunk.js"
+  },
+  {
+    "revision": "a76e3926bc7b05825602",
+    "url": "/reactlivedspservices/static/js/runtime~main.0870c760.js"
+  },
+  {
+    "revision": "30ab47529a87b19b9cb6",
+    "url": "/reactlivedspservices/static/css/2.258b1fd9.chunk.css"
+  },
+  {
+    "revision": "30ab47529a87b19b9cb6",
+    "url": "/reactlivedspservices/static/js/2.f354170d.chunk.js"
+  },
+  {
+    "revision": "8d8a49eda3341411a0f11446a2573cad",
+    "url": "/reactlivedspservices/static/media/img2.8d8a49ed.svg"
+  },
+  {
+    "revision": "c1e3fa7e27e65bb64ac8411b3accb5bb",
+    "url": "/reactlivedspservices/static/media/img1.c1e3fa7e.svg"
+  },
+  {
+    "revision": "4cc7caea5b452f1ca109dbf793425dac",
+    "url": "/reactlivedspservices/static/media/mobile.4cc7caea.svg"
+  },
+  {
+    "revision": "30884d454831d5b4948880561a0a9840",
+    "url": "/reactlivedspservices/static/media/digital.30884d45.svg"
+  },
+  {
+    "revision": "a793e398b69071959d98f0b8149379e5",
+    "url": "/reactlivedspservices/static/media/web.a793e398.svg"
+  },
+  {
+    "revision": "49b20d4f5ca9acb8845451d83e44aec7",
+    "url": "/reactlivedspservices/static/media/it_suport.49b20d4f.svg"
+  },
+  {
+    "revision": "b2e20d287aabbdfc29682810ce94eb51",
+    "url": "/reactlivedspservices/static/media/photography.b2e20d28.svg"
+  },
+  {
+    "revision": "746e6ff4d76806b04257ad4be1d20005",
+    "url": "/reactlivedspservices/static/media/tutor.746e6ff4.svg"
+  },
+  {
+    "revision": "0f2e3584155bf6297e5d84d6e28beca1",
+    "url": "/reactlivedspservices/static/media/kris.0f2e3584.jpeg"
+  },
+  {
+    "revision": "cb2b37441afb48f078ccdb9f751a1a01",
+    "url": "/reactlivedspservices/static/media/banty.cb2b3744.jpeg"
+  },
+  {
+    "revision": "e2ca752c78de2797f79eff353adda901",
+    "url": "/reactlivedspservices/index.html"
+  }
+];
